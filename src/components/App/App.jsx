e593d0dfeeedd41fc8d590321eddba45defdb57d
Lesia <150070945+Cassel444@users.noreply.github.com>
2024-06-04 12:28:58 +0300

    create components and pages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,23 @@
-import "./App.css";
-function App() {}
+import { Route, Routes } from "react-router-dom";
+import css from "./App.module.css";
+import Navigation from "../Navigation/Navigation";
+import HomePage from "../../pages/HomePage";
+import MoviesPage from "../../pages/MoviesPage";
+import MovieDetailsPage from "../../pages/MovieDetailsPage";
+import NotFoundPage from "../../pages/NotFoundPage";
+
+function App() {
+  return (
+    <div className={css.box}>
+      <Navigation />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:movieId" element={<MovieDetailsPage />}></Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
+}
 export default App;
